c796d9e63306368b4fc4a8a2f8348022be2493da
dkersten <dkersten1994@gmail.com>
2020-09-22 21:41:31 -0400

    added contact page using mdx

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,16 @@ module.exports = {
         title: 'Front End Masters Gatsby Workshop',
         description: 'Site working with Gatsby and GraphQL'
     },
-    plugins: ['gatsby-plugin-emotion', 'gatsby-plugin-react-helmet'],
+    plugins: [
+        'gatsby-plugin-emotion',
+        'gatsby-plugin-react-helmet',
+        {
+            resolve: 'gatsby-plugin-mdx',
+            options: {
+                defaultLayouts: {
+                    default: require.resolve('./src/components/layout.js'),
+                },
+            },
+        },
+    ],
 }
