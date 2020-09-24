501f798ef830e9ea9887e0c62b4eab60f0b514dd
dkersten <dkersten1994@gmail.com>
2020-09-24 18:46:33 -0400

    post photos added, fully responsive

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,9 @@ module.exports = {
                 defaultLayouts: {
                     default: require.resolve('./src/components/layout.js'),
                 },
+                gatsbyRemarkPlugins: [
+                    { resolve: 'gatsby-remark-images' }
+                ]
             },
         },
         {
--- src/hooks/use-posts.js
+++ src/hooks/use-posts.js
@@ -3,23 +3,34 @@ import { graphql, useStaticQuery } from 'gatsby';
 const usePosts = () => {
     const data = useStaticQuery(graphql`
         query {
-            allMdx {
+        allMdx {
             nodes {
-                frontmatter {
+            frontmatter {
                 title
-                slug
                 author
+                slug
+                image {
+                sharp: childImageSharp {
+                    fluid(
+                    maxWidth: 100
+                    maxHeight: 100
+                    ) {
+                    ...GatsbyImageSharpFluid_withWebp
+                    }
                 }
-                excerpt
+                }
             }
+            excerpt
             }
         }
-    `)
+        }
+    `);
 
     return data.allMdx.nodes.map(post => ({
         title: post.frontmatter.title,
         author: post.frontmatter.author,
         slug: post.frontmatter.slug,
+        image: post.frontmatter.image,
         excerpt: post.excerpt,
     }))
 }
